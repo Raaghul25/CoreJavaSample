3caccc35482580b6f10172991246dafd5a2d2e81
VASANTH <VASANTH@DESKTOP-7N1BTIQ.mshome.net>
2024-03-11 16:14:10 +0530

    It is my Java Program

---
+++ CoreJavaSamples/src/Day3_JavaBasics/user.java
@@ -0,0 +1,13 @@
+package Day3_JavaBasics;
+import java.util.*;
+public class user 
+{
+public static void main(String args[])
+{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter your name:");
+	String n=sc.nextLine();
+	System.out.println("Hello "+n);
+	
+}
+}
